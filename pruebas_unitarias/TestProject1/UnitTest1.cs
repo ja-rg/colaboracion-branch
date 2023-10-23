40680230c786cfba15200a8be093497511a0ab39
Alejandro Rosales <ja.rg@lasallistas.org.mx>
2023-10-23 15:55:47 -0600

    Añadiendo logs a las pruebas unitarias

--- pruebas_unitarias/TestProject1/UnitTest1.cs
+++ pruebas_unitarias/TestProject1/UnitTest1.cs
@@ -1,55 +1,83 @@
+using log4net;
+using log4net.Config;
+
 namespace pruebas_unitarias
 {
     public class Tests
     {
-        private Funciones funciones { get; set; } = null!;
+        private Funciones Funciones { get; set; } = null!;
+        private static readonly ILog log = LogManager.GetLogger(typeof(Tests));
+
         [SetUp]
         public void Setup()
         {
-            funciones = new Funciones();
+            XmlConfigurator.Configure(new FileInfo("log4net-config.xml"));
+            Funciones = new Funciones();
+            log.Info("Inicialización de las pruebas.");
         }
 
         [Test]
         public void EsPar()
         {
             int a = 10, b = 21;
+
+            log.Info("Iniciando prueba EsPar.");
+
             Assert.Multiple(() =>
             {
                 Assert.That(Funciones.EsPar(a), Is.True);
                 Assert.That(Funciones.EsPar(b), Is.False);
             });
+
+            log.Info("Finalizando prueba EsPar.");
         }
 
         [Test]
         public void EsDivisibleEntre3()
         {
             int a = 10, b = 21;
+
+            log.Info("Iniciando prueba EsDivisibleEntre3.");
+
             Assert.Multiple(() =>
             {
                 Assert.That(Funciones.EsDivisibleEntre3(a), Is.False);
                 Assert.That(Funciones.EsDivisibleEntre3(b), Is.True);
             });
+
+            log.Info("Finalizando prueba EsDivisibleEntre3.");
         }
 
         [Test]
         public void EsDivisibleEntre4()
         {
             int a = 12, b = 21;
+
+            log.Info("Iniciando prueba EsDivisibleEntre4.");
+
             Assert.Multiple(() =>
             {
                 Assert.That(Funciones.EsDivisibleEntre4(a), Is.True);
                 Assert.That(Funciones.EsDivisibleEntre4(b), Is.False);
             });
+
+            log.Info("Finalizando prueba EsDivisibleEntre4.");
         }
+
         [Test]
         public void EsDivisibleEntre5()
         {
             int a = 10, b = 21;
+
+            log.Info("Iniciando prueba EsDivisibleEntre5.");
+
             Assert.Multiple(() =>
             {
                 Assert.That(Funciones.EsDivisibleEntre5(a), Is.True);
                 Assert.That(Funciones.EsDivisibleEntre5(b), Is.False);
             });
+
+            log.Info("Finalizando prueba EsDivisibleEntre5.");
         }
     }
 }
--- pruebas_unitarias/pruebas_unitarias/Program.cs
+++ pruebas_unitarias/pruebas_unitarias/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using log4net;
+using log4net.Config;
+using static System.Net.Mime.MediaTypeNames;
+
+
+XmlConfigurator.Configure(new FileInfo("log4net-config.xml"));
+ILog logger = LogManager.GetLogger(typeof(Program));
+
+logger.Info("Programa iniciando");
 Console.WriteLine("Hello, World!");
+logger.Info("Programa finalizando");
